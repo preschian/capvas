241544b1163f404133b8e82aefe8868051286cd0
Preschian Febryantara <preschian@users.noreply.github.com>
2024-07-10 16:10:31 +0700

    refactor(screenshot): add error handling and browser closure in try-catch block

--- utils/screenshot.ts
+++ utils/screenshot.ts
@@ -4,29 +4,39 @@ export default async function ({ width = 600, height = 600, url = '', selector =
   // Launch the browser
   const browser = await launch({})
 
-  // Open a new page
-  const page = await browser.newPage()
-  await page.goto(url, { timeout: 30 * 10 * 1000 })
-  await page.setViewport({
-    height,
-    width,
-  })
+  try {
+    // Open a new page
+    const page = await browser.newPage()
+    await page.goto(url, { timeout: 30 * 10 * 1000 })
+    await page.setViewport({
+      height,
+      width,
+    })
 
-  // Delay n seconds to wait for the page to load
-  if (delay) {
-    await new Promise((resolve) => setTimeout(resolve, delay))
+    // Delay n seconds to wait for the page to load
+    if (delay) {
+      await new Promise((resolve) => setTimeout(resolve, delay))
+    }
+
+    // Take a screenshot of the element
+    const element = await page.waitForSelector(selector, { timeout: 30 * 10 * 1000 })
+    const image = await element?.screenshot()
+
+    // Close the browser
+    await browser.close()
+
+    return new Response(image, {
+      headers: {
+        'Content-Type': 'image/png',
+      },
+    })
+  } catch (error) {
+    browser.close()
+    return new Response(JSON.stringify({ error: (error as Error).message }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
   }
-
-  // Take a screenshot of the element
-  const element = await page.waitForSelector(selector, { timeout: 30 * 10 * 1000 })
-  const image = await element?.screenshot()
-
-  // Close the browser
-  await browser.close()
-
-  return new Response(image, {
-    headers: {
-      'Content-Type': 'image/png',
-    },
-  })
 }
